a76b2495af9494b8b829fcb6de9e966813570669
Ruslan <ruslan.shtybaev@gmail.com>
2022-02-08 11:52:16 +0300

    1 setup design main.storyboard 2. setup view of views and button

--- SvetoforExercise/ViewController.swift
+++ SvetoforExercise/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var lightsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getNewShape(colorView: redView)
+        getNewShape(colorView: yellowView)
+        getNewShape(colorView: greenView)
+        lightsButton.layer.cornerRadius = 10
+    }
+    
+    func getNewShape(colorView: UIView) {
+        colorView.layer.cornerRadius = colorView.frame.height/2
+        colorView.alpha = 0.3
     }
 
-
+    @IBAction func lightsChangingButton() {
+        }
 }
 
